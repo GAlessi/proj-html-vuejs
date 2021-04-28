b323234ebce83a986dae6836e8344f9d7347807f
Gabriele Alessi <89archaon89@gmail.com>
2021-04-28 23:08:18 +0200

    dropdown nav menu

--- src/app.js
+++ src/app.js
@@ -4,7 +4,83 @@ function initVue() {
 
         data:{
             countdown:"",
-            links:["Home", "Pages", "Courses", "Features", "Blog", "Shop"],
+            links:[
+                {nome:"Home",
+                    drop:[
+                        "MaxCoach Education",
+                        "Course Portal",
+                        "Distant Learning",
+                        "Multimedia Pedagogy",
+                        "Modern Schooling",
+                        "Remote Training",
+                        "Health Coaching",
+                        "Gym Coaching",
+                        "Business"
+                    ],
+                    drop2:[
+                        "Artist",
+                        "Kitchen Coach",
+                        "Motivation",
+                        "Dancing",
+                        "Guitar",
+                        "Yoga",
+                        "Photography",
+                        "Personal Finance",
+                    ]
+                },
+                {nome:"Pages",
+                    drop:[
+                        "Start Here",
+                        "Success Story",
+                        "About me",
+                        "About us",
+                        "About us 01",
+                        "About us 02",
+                        "About us 03",
+                        "Contact me",
+                        "Contact us",
+                        "Purchase Guide",
+                        "Privacy Policy",
+                        "Terms of Service"
+                    ]
+                },
+                {nome:"Courses",
+                    drop:[
+                        "Courses Grid 01",
+                        "Courses Grid 02",
+                        "Courses Grid 03",
+                        "Membership Levels",
+                        "Become a Teacher",
+                        "Profile",
+                        "Checkout",
+                        "Single Layout"
+                    ]
+                },
+                {nome:"Features",
+                    drop:[
+                        "Events",
+                        "Zoom Meetings"
+                    ]
+                },
+                {nome:"Blog",
+                    drop:[
+                        "Blog Grid",
+                        "Blog Masonry",
+                        "Blog Classic",
+                        "Blog List"
+                    ]
+                },
+                {nome:"Shop",
+                    drop:[
+                        "Shop Left Sidebar",
+                        "Shop Right Sidebar",
+                        "Cart",
+                        "Wishlist",
+                        "Single Product"
+                    ]
+                }
+
+            ],
             socials:["fa-twitter", "fa-facebook", "fa-instagram", "fa-linkedin"],
             explore:["Start here","Success story","Blog","Courses","Contact us"],
             information:["Membership","Purchase guide","Privacy policy","Terms of services"]
